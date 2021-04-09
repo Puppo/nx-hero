31a6058aadc9b7c643c0ed458bcf2d3f97bd44d5
Luca Del Puppo <luca@delpuppo.net>
2021-04-09 16:51:11 +0200

    be deploy

--- apps/hero-api/src/main.ts
+++ apps/hero-api/src/main.ts
@@ -1,21 +1,41 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { ServerlessNestjsApplicationFactory } from 'serverless-lambda-nestjs';
 import { AppModule } from './app/app.module';
 
-async function bootstrap() {
+const globalPrefix = 'api';
+export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  app.setGlobalPrefix('api');
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
 }
 
-bootstrap();
+// Run Nestjs application locally
+if (process.env.NX_CLI_SET) {
+  bootstrap();
+}
+
+// Run Nestjs application in AWS Lambda
+export const handler: APIGatewayProxyHandler = async (event, context) => {
+  const app = new ServerlessNestjsApplicationFactory<AppModule>(
+    AppModule,
+    {
+      // NestFactory.create's option object
+      cors: {
+        origin: '*',
+        allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+      },
+    },
+    (app) => {
+      app.enableCors();
+      app.setGlobalPrefix(globalPrefix);
+      return app;
+    }
+  );
+  const result = await app.run(event, context);
+  return result;
+};
